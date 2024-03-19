4d2c1356a894cda5213b88f979ba42ee37e6d073
zdotdev <www.ragaszedrick25@gmail.com>
2024-03-19 23:40:01 +0800

    Added comments in componen.js in component life cycle

--- src/routes/componentLifeCycle/component.js
+++ src/routes/componentLifeCycle/component.js
@@ -1,7 +1,7 @@
 import { onMount, beforeUpdate } from 'svelte';
 export default function ComponentLifeCycle () {
     let elements = new Set();
-    beforeUpdate(() => {
+    beforeUpdate(() => { // triggered before the component is updated
         elements.forEach(element => {
             element.style.outlineColor = 'red';
 
@@ -10,16 +10,16 @@ export default function ComponentLifeCycle () {
             }, 1000);
         })
     });
-    onMount(() => {
+    onMount(() => { // triggered when the component is mounted to the dom
         elements.forEach(element => {
             element.style.outline = '2px solid';
         })
     });
-    return function elemUpdate(node) { 
+    return function elemUpdate(node) { // triggered when the element is mounted or updated to the dom
         elements.add(node);
         return {
             destroy() {
-                elements.delete(node);
+                elements.delete(node); // triggered right after the element is removed from the dom
             }
         }
     }

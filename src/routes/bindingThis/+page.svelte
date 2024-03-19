ba16607ca9e768ebd8fe968313febb9b1a1e37c6
zdotdev <www.ragaszedrick25@gmail.com>
2024-03-19 17:46:43 +0800

    Added binding this

--- src/routes/bindingThis/+page.svelte
+++ src/routes/bindingThis/+page.svelte
@@ -1,9 +1,24 @@
 <script>
+    import Component from "./component.svelte";
+    let inputElement;
 
+    function handleClick() {
+        inputElement.value = '';
+    }
+
+    let component
+
+    const must = "<svelte:options accessors/>"
 </script>
-<div class="flex flex-col gap-2 m-4">
-    <div class="flex flex-col gap-2">
-        <input type="text" class="outline outline-2 w-fit">
-        <button class="bg-slate-200 rounded-lg p-2 w-fit">Click me</button>
+
+<div class="m-4">
+    <p>Bind:This is a method of binding element to a variable.</p>
+    <div class="flex gap-2 items-center">
+        <input type="text" bind:this={inputElement}  class="outline outline-2 h-fit" />
+        <button class="bg-slate-200 rounded-lg p-2 h-fit" on:click={handleClick}>Clear</button>
     </div>
 </div>
+<p>To use and manipulate data directly from Component, you must use <strong>{must}</strong> element</p>
+<Component bind:this={component}/>
+<button on:click={() => {component.num++}}>Increment component num value</button>
+<button on:click={component.reset}>Reset</button>
---
+++ src/routes/bindingThis/component.svelte
@@ -0,0 +1,13 @@
+<svelte:options accessors/>
+<script>
+    export let num = 0 // export the variable to be used in the parent component
+
+    export function reset(){
+        num = 0
+    }
+
+</script>
+
+<div>
+    <p>{num}</p>
+</div>

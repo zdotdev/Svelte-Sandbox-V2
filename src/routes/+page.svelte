5217a7f15919c3afc1f9daced033f8c82da755a1
zdotdev <www.ragaszedrick25@gmail.com>
2024-03-25 22:32:12 +0800

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,5 +21,6 @@
     <a href="/await" class="bg-slate-200 rounded-lg p-4">Await</a>
     <a href="/key" class="bg-slate-200 rounded-lg p-4">Key</a>
     <a href="/context" class="bg-slate-200 rounded-lg p-4">Context</a>
-    <a href="/communicatingThroughContext" class="bg-slate-200 rounded-lg p-4">Communicating through Contex</a>
+    <a href="/communicatingThroughContext" class="bg-slate-200 rounded-lg p-4">Communicating through Context</a>
+    <a href="/introToStores" class="bg-slate-200 rounded-lg p-4">Intro to Stores</a>
 </div>
